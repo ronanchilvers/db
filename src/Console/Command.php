c83e1502737484a9232a4fe647ae750b0da33185
Ronan Chilvers <ronan@thelittledot.com>
2018-09-21 13:54:30 +0100

    Remove unused references

--- src/Console/Command.php
+++ src/Console/Command.php
@@ -6,7 +6,6 @@ use Symfony\Component\Console\Command\Command as BaseCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Finder\Finder;
 
 class Command extends BaseCommand
 {
